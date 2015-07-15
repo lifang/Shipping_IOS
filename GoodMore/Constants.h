14b6a98c402e93c9c8788c6f3f0ca1b1fe9c6041
798666 <2987607054@qq.com>
2015-07-15 14:20:16 +0800

    7.15mi

--- GoodMore/Constants.h
+++ GoodMore/Constants.h
@@ -31,9 +31,9 @@
 
 #define kImageName(name) [UIImage imageNamed:name]
 
-#define KServiceURL @"http://120.25.243.169:8888/HDDPlatForm/"
+//#define KServiceURL @"http://120.25.243.169:8888/HDDPlatForm/"
 
-//#define KServiceURL @"http://192.168.0.163:8080/HDDPlatForm/"
+#define KServiceURL @"http://192.168.0.163:8080/HDDPlatForm/"
 
 #define kAppVersionType  2   //版本更新
 
--- GoodMore/Controllers/MainViewController.m
+++ GoodMore/Controllers/MainViewController.m
@@ -94,7 +94,7 @@
     }
     
 
-    [self downloadGoodDetail];
+//    [self downloadGoodDetail];
     
     
     
